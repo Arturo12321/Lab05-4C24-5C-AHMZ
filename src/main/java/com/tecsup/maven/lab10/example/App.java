e4b7ad7ce44b9c82c9f689db40e1cb02ea8d7f3e
Arturo12321 <valerio.manrique@tecsup.edu.pe>
2022-10-24 15:25:03 -0500

    first commit

---
+++ src/main/java/com/tecsup/maven/lab10/example/App.java
@@ -0,0 +1,13 @@
+package com.tecsup.maven.lab10.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
